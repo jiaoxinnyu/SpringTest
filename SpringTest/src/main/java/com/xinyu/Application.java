ba1b66bd262ba09da7c56c30d9784fdb545b8349
jiaoxinyu <1721591714@qq.com>
2021-05-10 17:48:14 +0800

    调整分库分表代码配置

--- SpringTest/src/main/java/com/xinyu/Application.java
+++ SpringTest/src/main/java/com/xinyu/Application.java
@@ -4,15 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author jiaoxy
  */
 @SpringBootApplication
 @ComponentScan("com.xinyu")
-@MapperScan(basePackages = "com.xinyu.mapper")
-@EnableScheduling
+@MapperScan("com.xinyu.mapper")
+@EnableOpenApi
 public class Application {
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
